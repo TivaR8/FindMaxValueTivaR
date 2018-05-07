89e10ba28858a7c64fde712c35f0cf352c83f8db
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-07 13:10:10 -0400

    Update

--- FindMaxValueTivaR/FindMaxValueForm.cs
+++ FindMaxValueTivaR/FindMaxValueForm.cs
@@ -24,13 +24,51 @@ namespace FindMaxValueTivaR
             InitializeComponent();
         }
 
+        // Function: GetMaxValue
+        // Input: int[] tmpArrayOfIntegers - assume all values in the array are greater than 0
+        // Output: the max value in the array 
+        // Description: This function accepts an array of integers,...
+        // ... scans the array and returns the greatest value in the array.
+        private int GetMaxValue(int[] tmpArrayOfIntegers)
+        {
+
+
+            return tmpMaxValue;
+        }
+
         private void btnStart_Click(object sender, EventArgs e)
         {
             // declare local variables and constants 
             const int MAX_ARRAY_SIZE = 10;
             const int MAX_RANDOM_NUMBER = 500;
             int[] arrayOfIntegers = new int[MAX_ARRAY_SIZE];
-            int[] counter, randomNumber, maxValue;
+            int counter, randomNumber, maxValue;
+            Random randomNumberGenerator = new Random();
+
+            // Clear list box
+            this.lstNumberSet.Items.Clear();
+
+            // To generate the numbers
+            for (counter = 0; counter < MAX_ARRAY_SIZE; counter++)
+            {
+                // generate a new random number between 1 and the MAX_ARRAY_SIZE
+                randomNumber = randomNumberGenerator.Next(1, MAX_RANDOM_NUMBER + 1);
+
+                // Assign the random number to the cell at position "counter" in the array
+                arrayOfIntegers[counter] = randomNumber;
+
+                // add the random number to the list 
+                this.lstNumberSet.Items.Add(randomNumber);
+
+                // Refresh the form to display the new item in the list box
+                this.Refresh();
+
+                // To get the Max Value in the array
+                maxValue = GetMaxValue(arrayOfIntegers);
+
+                // Display the max value in the label
+                this.lblMaxValue.Text = "The max value is: " + maxValue;
+            }
         }
     }
 }
